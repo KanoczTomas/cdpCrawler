1abe05145a10779fcd4aa2ef8a1e76cf29316e0d
Tomas Kanocz <tomas.kanocz@cnl.sk>
2017-06-13 17:35:31 +0200

    worked on cdpCrawl, need to solve the qeuue push stuff:

--- js/cdpCrawl.js
+++ js/cdpCrawl.js
@@ -23,34 +23,38 @@ function cdpCrawl(hostname){
     var root; //the root node info will be held here
     
     
+    function crawlDevice(hostname){
+        return Promise.coroutine(function *gen(){
+            var result;
+
+            try{
+                result = yield findSnmpCommunity(hostname);
+            }
+            catch(err){
+                result = Promise.reject(err);
+            }
+
+            try{
+                result.cdpInfo = yield findCdpNeighbors(result.hostname, result.community)
+            }
+            return Promise.resolve(result);    
+        })();
+    }
+    
     function addToqueue(hostname){
         //will hate to check if the nostname is in list of devices crawled, 
         //if it is not will call cdpCrawl.queue.push()
         //the then after push must include the hostname in the list of crawled devices
+        if(cdpCrawl.listOfDevicesCrawled.infdexOf(hostname) === -1){
+            cdpCrawl.queue.push(crawlDevice(hostname))
+            .then(function(res){ //we add the hostname to listOfDevicesCrawled
+                 if(cdpCrawl.listOfDevicesCrawled.infdexOf(hostname) === -1){
+                     cdpCrawl.listOfDevicesCrawled.push(hostname);
+                 }
+                
+            });
+        }
     }
-    
-    //this should be a generic function for all jobs included to the queue, resolving to a promise which once filled goes to list of devices crawled
-    return Promise.coroutine(function *gen(){
-        var result;
-        
-        try{
-            root = yield findSnmpCommunity(hostname);
-        }
-        catch(err){
-            result = Promise.reject(err);
-        }
-        
-        try{
-            root.cdpInfo = yield findCdpNeighbors(root.hostname, root.community)
-        }
-        
-        //nezabudn pridat root hostname do list of devices crawled
-        
-        
-        return Promise.resolve(result);
-        
-        
-    })();
 }
 
 
